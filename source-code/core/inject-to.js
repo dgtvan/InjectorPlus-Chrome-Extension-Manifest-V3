4224e43bd0bf5caefbb920197a6ecb3e9a93e5f5
Van Dang <dgtvan@gmail.com>
2024-11-08 14:15:13 +0700

    x

--- source-code/core/inject-to.js
+++ source-code/core/inject-to.js
@@ -3,14 +3,5 @@
 /* they should be in the 'hostname' format. •••••••••••• */
 /* ••••••••••••••••••••••••••••••••••••••••••••••••••••• */
 const injectTo = [
-    'linktr.ee',
-    'example.com',
-    'twitter.com',
-    'chat.openai.com',
-    'www.ea.com',
-    'www.youtube.com',
-    'www.facebook.com',
-    'www.wikipedia.org',
-    '127.0.0.1',
-    '192.168.1.1'
+    'www.scrumpoker-online.org'
 ];
