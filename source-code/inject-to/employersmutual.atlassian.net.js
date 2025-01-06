f2bb6c740cc99efed179dfa9d178e6a5cb8c8f8a
Van Dang <dgtvan@gmail.com>
2025-01-06 14:58:05 +0700

    happy new year  ~

--- source-code/inject-to/employersmutual.atlassian.net.js
+++ source-code/inject-to/employersmutual.atlassian.net.js
@@ -15,7 +15,10 @@
             }
         
             for (const target of targets) {
-                target.style.background = 'url(https://i.giphy.com/CkBzt6qXCCXw4.webp) center center'
+                //target.style.background = 'url(https://i.giphy.com/CkBzt6qXCCXw4.webp) center center'
+                target.style.background = 'url(https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzJ3eHo0eTFuMHFveHRzbWVrczNvdnhjOWphNThkNDg5cjRoZXJ5aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xjreOgT7TKfETjysdr/giphy.webp)'
+                target.style.backgroundPosition = "60% 230%";
+                target.style.backgroundSize = "80%";
             }
             
             await sleep(1000);
